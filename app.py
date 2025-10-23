679b4530bf096c58d871c93e7462dce5122dd4f8
Murat Karakelle <muratkarakelle06@gmail.com>
2025-10-23 19:37:58 +0300

    Initial commit - Flask API

--- app.py
+++ app.py
@@ -163,4 +163,6 @@ def hesapla_xml():
     return Response(xml_output, mimetype='application/xml')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
